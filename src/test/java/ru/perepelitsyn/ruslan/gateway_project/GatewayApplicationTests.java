692d573a7bc24033f71a6d3a9d554a0e7d0a3c12
RuslanPerepelicyn <69298060+RuslanPerepelicyn@users.noreply.github.com>
2020-08-25 16:54:48 +0300

    initial commit

---
+++ src/test/java/ru/perepelitsyn/ruslan/gateway_project/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.perepelitsyn.ruslan.gateway_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
